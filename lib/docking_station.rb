df60858617921d70c06f22d7d52640b6851f2322
Corinne <corinne.boesch@gmail.com>
2021-07-27 17:47:36 +0100

    Updating release_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,6 @@
 class DockingStation
-  def initialize
-    docking_station = DockingStation.new
+  def release_bike
+    
   end
 end
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,7 @@
 require 'docking_station.rb'
 
 describe DockingStation do
+   it 'release bike' do
+     expect(DockingStation).to eq('release bike')
+  end
 end
