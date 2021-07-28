601e1f09b99ec02a111eebe54572355d33026e60
William <williamjday11@gmail.com>
2021-07-28 10:00:09 +0000

    Files corrected after merge

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,13 +1,6 @@
 class DockingStation
   def release_bike
-<<<<<<< HEAD
     'release bike'
-=======
-<<<<<<< HEAD
-    
-=======
->>>>>>> 15ff8750ab4f106ebaa9df201ff011f7e01fe17f
->>>>>>> 79c0144c6e4b197f1c6f436b4318640b589aba9e
   end
 end
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,16 +1,7 @@
 require 'docking_station.rb'
 
 describe DockingStation do
-<<<<<<< HEAD
-   it 'release bike' do
-     expect(DockingStation).to eq('release bike')
-=======
   it 'release bike' do
-<<<<<<< HEAD
     expect(DockingStation.new.release_bike).to eq ('release bike')
-=======
-    expect(release_bike).to eq
->>>>>>> 15ff8750ab4f106ebaa9df201ff011f7e01fe17f
->>>>>>> 79c0144c6e4b197f1c6f436b4318640b589aba9e
   end
 end
